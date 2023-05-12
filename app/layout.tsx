68236bff0e4d96def19423fe7d37d4c28127f41f
bearzhao007 <28466864@qq.com>
2023-05-12 15:58:27 +0800

    1

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "./styles/globals.scss";
+import "./styles/markdown.scss";
+import "./styles/highlight.scss";
+import { getBuildConfig } from "./config/build";
+
+const buildConfig = getBuildConfig();
+
+export const metadata = {
+  title: "憨憨ChatGPT机器人",
+  description: "憨憨chatGPT机器人",
+  appleWebApp: {
+    title: "憨憨ChatGPT机器人",
+    statusBarStyle: "default",
+  },
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="theme-color"
+          content="#fafafa"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#151515"
+          media="(prefers-color-scheme: dark)"
+        />
+        <meta name="version" content={buildConfig.commitId} />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="preconnect" href="https://fonts.proxy.ustclug.org"></link>
+        <link
+          href="https://fonts.proxy.ustclug.org/css2?family=Noto+Sans+SC:wght@300;400;700;900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
